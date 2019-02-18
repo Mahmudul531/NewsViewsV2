9c2450703c53af8009527de86b5105e636c8b22d
Mahmudul Hassan <mahmudul531@gmail.com>
2019-02-18 18:11:30 +0600

    Update

---
+++ app/src/main/java/com/mahmudul/NewsViews/TEST.java
@@ -0,0 +1,14 @@
+package com.mahmudul.NewsViews;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TEST extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+    }
+}
